904bedfe58c4e96887408adddd230df316919cc4
ben <ben@bens-MacBook-Pro.local>
2026-03-23 19:59:47 -0400

    Remove SQLite option for PostgreSQL

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ load_dotenv()
 
 app = FastAPI()
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://snippet_db_19se_user:snippet_db_19se_user@localhost:5432/snippet_db_19se")
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
